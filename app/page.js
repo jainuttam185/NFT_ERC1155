8b79270fe3fa3333db81aee63562bd29bcb920eb
jainuttam185 <jainuttam185@gmail.com>
2024-02-17 16:29:49 +0530

    third commit

--- app/page.js
+++ app/page.js
@@ -1,52 +1,36 @@
 "use client";
 import styles from "./page.module.css";
 import React, { Component } from "react";
-import { ethers } from "ethers";
-import Pool from "./artifacts/contracts/Pool.sol/Pool.json";
-import ERC20 from "./artifacts/contracts/ERC20.sol/ERC20.json";
+const ethers = require("ethers");
+import nft from "../frontend/app/artifacts/contracts/nft.sol/nft.json";
+import Image from "next/image";
+import spidermanImage from './spiderman.jpeg';
 
 
 class CampaignIndex extends Component {
   state = {
-    token: "", outputValue: "", reserveIn: 0, reserveOut: 0, contract:"",Token1Contract:"",Token2Contract:"",Provider:"",contractAddress: "0x6ae5242BACC798DaCC470832F33c9A1E64166c0f",Token1Address:"0x1928496a8E34967F02eAF527Fe8A4EaC2EA8E646",Token2Address:"0x84933aD21c5e47d390061877ad7B149e042dB983"
-  };
-
-
+  token: 2, outputValue: "", contract:"", Provider:"",ipfsaddress:"ipfs://bafkreidblxpobb5frd57djj43mavu2ixtbyrofqq3ieflpwavaoqq524yq" ,toaddress:"0xf261F307159B06BeAAB840fe4281d456F5156A50", contractAddress: "0x96aE869d6b5E45eD78609747775b835947491FDe"
+};
 
   async componentDidMount() {
 
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const provider = new ethers.BrowserProvider(window.ethereum);
       if (provider) {
         await provider.send("eth_requestAccounts", []);
-        const signer = provider.getSigner();
+        const signer = await provider.getSigner();
         const address = await signer.getAddress();
         //setAccount(address);
         // const contractAddress = "0xcC680Ce60E640F8BEF955AC5fDe00F4700DC97D3";
         console.log(provider);
         const contract = new ethers.Contract(
           this.state.contractAddress,
-          Pool.abi,
+          nft.abi,
           signer
         );
-        const Token1Contract = new ethers.Contract(
-          this.state.Token1Address,
-          ERC20.abi,
-          signer
-        );
-        const Token2Contract = new ethers.Contract(
-          this.state.Token2Address,
-          ERC20.abi,
-          signer
-        );
+        
         //this.setState.contract=contract;
         this.setState({
           contract: contract
-        });
-        this.setState({
-          Token1Contract: Token1Contract
-        });
-        this.setState({
-          Token2Contract: Token2Contract
         });
         this.setState({
           Provider:signer
@@ -54,16 +38,6 @@ class CampaignIndex extends Component {
         // this.setState.Token1Contract=Token1Contract;
         // this.setState.Token2Contract=Token2Contract;
         // this.setState.Provider=signer;
-
-        this.setState({
-          reserveIn: await contract.reserve0()
-        });
-        this.setState({
-          reserveOut: await contract.reserve1()
-        });
-        this.setState.reserveOut = await contract.reserve1();
-        console.log("line1", parseInt(this.state.reserveIn));
-        console.log("line2", parseInt(this.state.reserveOut));
       } else {
         alert("Metamask is not installed.");
       }
@@ -82,10 +56,8 @@ class CampaignIndex extends Component {
   onSubmit = async (event) => {
     console.log(this.state.contract)
     event.preventDefault();
-    const approve = await this.state.Token2Contract.approve(this.state.contractAddress,this.state.token);
-    await approve.wait();
-    const swap = await this.state.contract.swap(this.state.Token2Address, this.state.token);
-    await swap.wait();
+    const mint = await this.state.contract.mint(this.state.toaddress, this.state.ipfsaddress);
+    await mint.wait();
   };
 
 
@@ -105,36 +77,13 @@ class CampaignIndex extends Component {
   render() {
     return (
       <div className={styles.full}>
-        <div className={styles.header}><h2 className={styles.header2}>UTTAM SWAP</h2></div>
+        <div className={styles.header}><h2 className={styles.header2}>UTTAM MINT NFT</h2></div>
         <div className={styles.top}>
           <div className={styles.box}>
-            <div className={styles.token1}>Token 2</div>
-            <input
-              type="number"
-              className={styles.input1}
-              placeholder="0"
-              value={this.state.token}
-              onChange={event => this.setState({
-                token: event.target.value, outputValue:
-                  this.state.reserveOut * event.target.value * 1.13 / this.state.reserveIn
-              })}
-            />
-            {console.log("hi")}
-            {console.log("render", this.state.reserveIn, this.state.reserveOut)}
-            <div className={styles.token1}>Token 1</div>
-            <div className={styles.buttondiv}>
-              <input
-                type="number"
-                className={styles.input2}
-                placeholder="0"
-                value={this.state.outputValue}
-                readOnly
-              />
-              <div>
-                <button className={styles.button} onClick={this.onSubmit}>Swap</button>
+            <div className={styles.token1}>SpiderMan NFT</div>
+                 <div className={styles.imageContainer}><Image src={spidermanImage} alt="Spiderman" className={styles.image}/></div>
+                <button className={styles.button} onClick={this.onSubmit}>MINT</button>
               </div>
-            </div>
-          </div>
         </div>
       </div>
     );
