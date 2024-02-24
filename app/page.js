5c484f232628a9ad720d50bcb23cbbc349236d42
jainuttam185 <jainuttam185@gmail.com>
2024-02-24 09:20:24 +0530

    Seventh Commit

--- app/page.js
+++ app/page.js
@@ -1,21 +1,31 @@
 "use client";
-import styles from "./page.module.css";
-import React, { Component } from "react";
-const ethers = require("ethers");
-import nft from "../frontend/app/artifacts/contracts/nft.sol/nft.json";
 import Image from "next/image";
-import spidermanImage from './spiderman.jpeg';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import { Component } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import nft from "../frontend/app/artifacts/contracts/nft.sol/nft.json";
+import styles from "./page.module.css";
+import spidermanImage from "./spiderman.jpeg";
+const ethers = require("ethers");
 
 class CampaignIndex extends Component {
   state = {
-    token: 0, outputValue: "", contract: "", Provider: "", ipfsaddress: "ipfs://bafkreidblxpobb5frd57djj43mavu2ixtbyrofqq3ieflpwavaoqq524yq", toaddress: "0xf261F307159B06BeAAB840fe4281d456F5156A50", contractAddress: "0x8F2e1016c2F0Ea42603A96ef37523E6a8063c4F5", loading: false, isloading:false, burnloading:false
+    token: 0,
+    outputValue: "",
+    contract: "",
+    Provider: "",
+    ipfsaddress:
+      "ipfs://bafkreidblxpobb5frd57djj43mavu2ixtbyrofqq3ieflpwavaoqq524yq",
+    toaddress: "0xf261F307159B06BeAAB840fe4281d456F5156A50",
+    contractAddress: "0xf8d1f53b958d76Aa1297389166BeC71124fcaC83",
+    loading: false,
+    isloading: false,
+    burnloading: false,
+    mintPrice: 0,
+    burnPrice: 0,
   };
 
   async componentDidMount() {
-
     const provider = new ethers.BrowserProvider(window.ethereum);
     if (provider) {
       await provider.send("eth_requestAccounts", []);
@@ -32,22 +42,25 @@ class CampaignIndex extends Component {
 
       //this.setState.contract=contract;
       this.setState({
-        contract: contract
+        contract: contract,
       });
       this.setState({
-        Provider: signer
+        Provider: signer,
+      });
+      this.setState({
+        mintPrice: await contract.Price(),
+      });
+      this.setState({
+        burnPrice: await contract.BuurnPrice(),
       });
       // this.setState.Token1Contract=Token1Contract;
       // this.setState.Token2Contract=Token2Contract;
       // this.setState.Provider=signer;
+      console.log("line1", parseInt(this.state.mintPrice));
     } else {
       alert("Metamask is not installed.");
     }
 
-
-    // this.setState({
-    //   reserveIn: await contract.reserve0()
-    // });
     // this.setState({
     //   reserveOut: await contract.reserve1()
     // });
@@ -57,102 +70,101 @@ class CampaignIndex extends Component {
   }
   onSubmit = async (event) => {
     try {
-      console.log(this.state.contract)
+      console.log(this.state.contract);
       event.preventDefault();
       this.setState({ loading: true });
       this.setState({ isloading: true });
       const price = await this.state.contract.Price();
-      const mint = await this.state.contract.mint(this.state.ipfsaddress, { value: price });
+      const mint = await this.state.contract.mint(this.state.ipfsaddress, {
+        value: price,
+      });
       this.state.loading && toast("Please wait for few seconds");
       //toast(mint ,{pending:"Please wait for few seconds",success:"Mint Successfull",error:"Sorry,Mint failed"});
       await mint.wait();
       // this.setState({ loading: false });
       toast("Mint Successfull");
+      this.state.mintPrice = await this.state.contract.Price();
+      this.state.burnPrice = await this.state.contract.BuurnPrice();
     } catch (error) {
       const errorMessage = error.message.split("(")[0];
       const fullMessage = errorMessage[0].toUpperCase() + errorMessage.slice(1);
       toast(fullMessage);
-    } 
-    finally {
+    } finally {
       this.setState({ loading: false });
       this.setState({ isloading: false });
     }
-
   };
 
   onSubmitBurn = async (event) => {
-    try{
-    console.log(this.state.contract)
-    event.preventDefault();
-    this.setState({ loading: true });
-    this.setState({ burnloading: true });
-    const burn = await this.state.contract.burn(this.state.token);
-    this.state.loading && toast("Please wait for few seconds");
-    await burn.wait();
-    toast("Burn Successfull");}catch(error){
+    try {
+      console.log(this.state.contract);
+      event.preventDefault();
+      this.setState({ loading: true });
+      this.setState({ burnloading: true });
+      const burn = await this.state.contract.burn();
+      this.state.loading && toast("Please wait for few seconds");
+      await burn.wait();
+      toast("Burn Successfull");
+      this.state.mintPrice = await this.state.contract.Price();
+      this.state.burnPrice = await this.state.contract.BuurnPrice();
+    } catch (error) {
       const errorMessage = error.message.split("(")[0];
       const fullMessage = errorMessage[0].toUpperCase() + errorMessage.slice(1);
       toast(fullMessage);
-    }
-    finally {
+    } finally {
       this.setState({ loading: false });
       this.setState({ burnloading: false });
     }
   };
 
-
-
-  /* static async getIntialProps(){
-
-      onSubmit=async (event)=>{
-           event.preventDefault();
-
-           const accounts=await web3.eth.getAccounts();
-           await factory.methods.swap(this.state.token)
-           .send({
-               from : accounts[0]
-           });
-      };
-   }*/
   render() {
     return (
       <div className={styles.full}>
         <ToastContainer />
-        <div className={styles.header}><h2 className={styles.header2}>UTTAM MINT NFT</h2></div> 
+        <div className={styles.header}>
+          <h2 className={styles.header2}>UTTAM MINT NFT</h2>
+        </div>
         <div className={styles.top}>
           <div className={styles.box}>
             <div className={styles.token1}>SpiderMan NFT</div>
-            <div className={styles.imageContainer}><Image src={spidermanImage} alt="Spiderman" className={styles.image} /></div>
-            {/* {this.state.isloading ? <button disabled={this.state.isloading} className={styles.button}><p className={styles.spinner}></p></button> : <button className={styles.button} onClick={this.onSubmit}>MINT</button>} */}
-            <button disabled={this.state.isloading} className={styles.button} onClick={this.onSubmit}> 
-            {this.state.isloading ? <p className={styles.spinner}></p> : "Mint" }
-            </button>
-            <div className={styles.token1}>Enter the Token Id you want to Burn</div>
-            <div className={styles.burn}>
-              <input
-                type="number"
-                className={styles.input1}
-                placeholder="0"
-                value={this.state.token}
-                onChange={event => this.setState({
-                  token: event.target.value
-                })}
+            <div className={styles.imageContainer}>
+              <Image
+                src={spidermanImage}
+                alt="Spiderman"
+                className={styles.image}
               />
-              <div>
-              <button disabled={this.state.burnloading} className={styles.buttonBurn} onClick={this.onSubmitBurn}> 
-            {this.state.burnloading ? <p className={styles.spinner}></p> : "Burn" }
+            </div>
+            <button
+              disabled={this.state.isloading}
+              className={styles.button}
+              onClick={this.onSubmit}
+            >
+              {this.state.isloading ? (
+                <p className={styles.spinner}></p>
+              ) : (
+                "Mint"
+              )}
             </button>
-                
+            <div className={styles.price}>
+                   <div>{this.state.mintPrice.toString()}</div>
+                   <div>{this.state.burnPrice.toString()}</div> 
+            </div>
+                <button
+                  disabled={this.state.burnloading}
+                  className={styles.buttonBurn}
+                  onClick={this.onSubmitBurn}
+                >
+                  {this.state.burnloading ? (
+                    <p className={styles.spinner}></p>
+                  ) : (
+                    "Burn"
+                  )}
+                </button>
               </div>
             </div>
           </div>
-        </div>
-      </div>
     );
   }
 }
 
 export default CampaignIndex;
-
-
-
--- contracts/nft.sol
+++ contracts/nft.sol
@@ -9,6 +9,7 @@ contract nft is ERC721URIStorage, Ownable {
     uint256 public totalSupply;
     uint256 listPrice = 0.001 ether;
     mapping(uint256 => address) public tokenOwner;
+    mapping(address => uint[]) public countOfTokenId;
 
     constructor() ERC721("Spiderman", "Spider") Ownable(msg.sender) {}
 
@@ -24,6 +25,23 @@ contract nft is ERC721URIStorage, Ownable {
         return price;
     }
 
+    function BuurnPrice() public view returns (uint){
+        if(totalSupply==0){
+            return 0;
+        }
+        uint burnsupply=totalSupply-1;
+        if (burnsupply == 0) {
+            return 100000000000000;
+        }
+        uint pricee = 1000000000000000000*(burnsupply)**2 / 8000;
+        return pricee;
+    }
+
+    function NumberOfTokens() public view returns (uint){
+        uint count = countOfTokenId[msg.sender].length;
+        return count;
+    }
+
     function mint(string calldata _uri) external payable {
         uint256 mintPrice = Price();
         require(msg.value == mintPrice, "please enter correct amount");
@@ -32,15 +50,16 @@ contract nft is ERC721URIStorage, Ownable {
         _mint(msg.sender, counter);
         _setTokenURI(counter, _uri);
         tokenOwner[counter] = msg.sender;
+        countOfTokenId[msg.sender].push(counter);
     }
 
-    function burn(uint256 _tokenId) external payable {
-        require(
-            tokenOwner[_tokenId] == msg.sender,
-            "You are not the owner of contract"
-        );
+    function burn() external payable {
+        require(countOfTokenId[msg.sender].length>0,"Sorry don't have enough tokens,Please mint");
+        uint length = countOfTokenId[msg.sender].length;
+        uint _tokenId = countOfTokenId[msg.sender][length-1];                           
         totalSupply--;
         _burn(_tokenId);
+        countOfTokenId[msg.sender].pop();
         payable(msg.sender).transfer(Price());
     }
 }
